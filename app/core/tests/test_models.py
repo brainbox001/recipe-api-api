4dc9580f859a44742c8b2dc3ae609a6a61696e61
Marcus Daniel <momsdboy@gmail.com>
2023-11-27 11:51:19 +0100

    blank line

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,7 +9,6 @@ from django.urls import reverse # noqa
 class ModelTests(TestCase):
     """Test models."""
 
-
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
         email = 'test@example.com'
